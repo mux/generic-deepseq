b7182c50d01f141466be69c3ae7c99f47f4c209b
Maxime Henrion <mhenrion@gmail.com>
2012-02-18 12:44:49 +0100

    Add a bunch of instances for primitive types, and remove a useless one.

--- Control/DeepSeq.hs
+++ Control/DeepSeq.hs
@@ -3,6 +3,8 @@ module Control.DeepSeq
   ( DeepSeq(..)
   ) where
 
+import Data.Int
+import Data.Word
 import GHC.Generics
 
 class DeepSeq a where
@@ -15,7 +17,16 @@ instance DeepSeq Char where deepseq = seq
 instance DeepSeq Double where deepseq = seq
 instance DeepSeq Float where deepseq = seq
 instance DeepSeq Int where deepseq = seq
-instance DeepSeq Ordering where deepseq = seq
+instance DeepSeq Int8 where deepseq = seq
+instance DeepSeq Int16 where deepseq = seq
+instance DeepSeq Int32 where deepseq = seq
+instance DeepSeq Int64 where deepseq = seq
+instance DeepSeq Word where deepseq = seq
+instance DeepSeq Word8 where deepseq = seq
+instance DeepSeq Word16 where deepseq = seq
+instance DeepSeq Word32 where deepseq = seq
+instance DeepSeq Word64 where deepseq = seq
+instance DeepSeq Integer where deepseq = seq
 instance DeepSeq () where deepseq = seq
 
 instance (DeepSeq a, DeepSeq b) => DeepSeq (a,b)
