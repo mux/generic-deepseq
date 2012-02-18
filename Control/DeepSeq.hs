9d9853b1b4510988d2b9ed1e87d9b9ca14e2ceb3
Maxime Henrion <mhenrion@gmail.com>
2012-02-18 12:39:10 +0100

    Make this code actually work. I don't really like having explicit instances for every primitive type, and considering whether I should reuse the NFData class from the primitive package to that end.

--- Control/DeepSeq.hs
+++ Control/DeepSeq.hs
@@ -1,23 +1,23 @@
-{-# LANGUAGE DefaultSignatures, FlexibleContexts #-}
+{-# LANGUAGE DefaultSignatures, FlexibleContexts, TypeOperators #-}
 module Control.DeepSeq
   ( DeepSeq(..)
   ) where
 
 import GHC.Generics
-import Control.DeepSeq.Internal
 
 class DeepSeq a where
   deepseq :: a -> b -> b
   default deepseq :: (Generic a, GDeepSeq (Rep a)) => a -> b -> b
-  deepseq x = gdeepseq (from x)
+  deepseq = gdeepseq . from
 
-instance DeepSeq Bool
-instance DeepSeq Char
-instance DeepSeq Double
-instance DeepSeq Float
-instance DeepSeq Int
-instance DeepSeq Ordering
-instance DeepSeq ()
+instance DeepSeq Bool where deepseq = seq
+instance DeepSeq Char where deepseq = seq
+instance DeepSeq Double where deepseq = seq
+instance DeepSeq Float where deepseq = seq
+instance DeepSeq Int where deepseq = seq
+instance DeepSeq Ordering where deepseq = seq
+instance DeepSeq () where deepseq = seq
+
 instance (DeepSeq a, DeepSeq b) => DeepSeq (a,b)
 instance (DeepSeq a, DeepSeq b, DeepSeq c) => DeepSeq (a,b,c)
 instance (DeepSeq a, DeepSeq b, DeepSeq c, DeepSeq d) => DeepSeq (a,b,c,d)
@@ -26,3 +26,22 @@ instance (DeepSeq a, DeepSeq b, DeepSeq c, DeepSeq d, DeepSeq e) =>
 instance (DeepSeq a, DeepSeq b, DeepSeq c, DeepSeq d, DeepSeq e, DeepSeq f) =>
           DeepSeq (a,b,c,d,e,f)
 instance DeepSeq a => DeepSeq [a]
+
+class GDeepSeq f where
+  gdeepseq :: f a -> b -> b
+
+instance GDeepSeq U1 where
+  gdeepseq = flip const
+
+instance DeepSeq a => GDeepSeq (K1 i a) where
+  gdeepseq = deepseq . unK1
+
+instance GDeepSeq a => GDeepSeq (M1 i c a) where
+  gdeepseq = gdeepseq . unM1
+
+instance (GDeepSeq a, GDeepSeq b) => GDeepSeq (a :*: b) where
+  gdeepseq (x :*: y) = gdeepseq x . gdeepseq y
+
+instance (GDeepSeq a, GDeepSeq b) => GDeepSeq (a :+: b) where
+  gdeepseq (L1 x) = gdeepseq x
+  gdeepseq (R1 x) = gdeepseq x
