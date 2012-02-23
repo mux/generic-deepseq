1f9e2ac9203a857d7298a8efadec99c16804311c
Maxime Henrion <mhenrion@gmail.com>
2012-02-23 21:06:42 +0100

    Add a simple benchmark to compare this implementation with the deepseq package.

---
+++ bench/Benchmark.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Criterion.Main
+import qualified "deepseq" Control.DeepSeq as D1
+import qualified "generic-deepseq" Control.DeepSeq as D2
+
+list :: Int -> [Int]
+list n = [1..10^n]
+
+main :: IO ()
+main = defaultMain
+         [ bgroup "deepseq"
+             [ bench "10^5 elems" $ whnf (uncurry D1.deepseq) (list 5,())
+             , bench "10^6 elems" $ whnf (uncurry D1.deepseq) (list 6,())
+             , bench "10^7 elems" $ whnf (uncurry D1.deepseq) (list 7,())
+             ]
+         , bgroup "generic-deepseq"
+             [ bench "10^5 elems" $ whnf (uncurry D2.deepseq) (list 5,())
+             , bench "10^6 elems" $ whnf (uncurry D2.deepseq) (list 6,())
+             , bench "10^7 elems" $ whnf (uncurry D2.deepseq) (list 7,())
+             ]
+         ]
